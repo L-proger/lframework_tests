91c96ed4ebe97e7f56fed62bd2f98e68fcd00e12
sergey.serb <l-proger@yandex.ru>
2017-09-16 14:12:28 +0300

    First unit test is working

---
+++ desktop/lframework_test/lframework_test/LFrameworkConfig.h
@@ -0,0 +1,8 @@
+#ifndef LFrameworkConfig_h__
+#define LFrameworkConfig_h__
+
+//#define LF_STM32
+//#define LF_USB_DEVICE
+//#define LF_THREADING
+
+#endif // LFrameworkConfig_h__
--- desktop/lframework_test/lframework_test/lframeworktest.cpp
+++ desktop/lframework_test/lframework_test/lframeworktest.cpp
@@ -1,6 +1,13 @@
+#include "lframework/UnitTest/UnitTest.h"
+#include <iostream>
+
+using namespace LFramework;
+
+TEST_IMPORT(BitField_8bit)
 
 int main()
 {
-    return 0;
+	auto result = TEST_RUN(BitField_8bit);
+	std::cin.get();
 }
 
