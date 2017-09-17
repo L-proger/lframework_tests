a1f1dae1be0206d2e4c7a7c8ac6093403fb19f2f
sergey.serb <l-proger@yandex.ru>
2017-09-17 18:55:55 +0300

    Added submodules

--- desktop/lframework_test/lframework_test/lframeworktest.cpp
+++ desktop/lframework_test/lframework_test/lframeworktest.cpp
@@ -1,16 +1,15 @@
 #include "lframework/UnitTest/UnitTest.h"
 #include "lframework/Thread/Thread.h"
 #include <iostream>
+#include "lframework/MCU/USBDevice/USBTypes.h"
 
 using namespace LFramework;
 
 TEST_IMPORT(BitField_8bit)
 TEST_IMPORT(Thread_Create)
 
-int main()
-{
-	auto result = TEST_RUN(BitField_8bit);
-	result = TEST_RUN(Thread_Create);
+int main(){
+	Testing::runAllTests();
 	std::cin.get();
 }
 
