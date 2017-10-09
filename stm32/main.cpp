f8044941df2c47ac636b2b9cfe7935096e402263
sergey.serb <l-proger@yandex.ru>
2017-10-10 02:44:21 +0300

    Nothing interesting

--- stm32/main.cpp
+++ stm32/main.cpp
@@ -17,6 +17,7 @@
 #include "ili9325/ili9325_fsmc.h"
 #include "lmath/lmath/lmath.h"
 #include "lframework/MCU/USBDevice/USBDevice.h"
+#include "lframework/MCU/USBDevice/UsbInternalFsDeviceDriver.h"
 #include "lframework/UnitTest/UnitTest.h"
 
 SRAM_HandleTypeDef hsram1;
@@ -34,7 +35,7 @@ extern "C" void _exit(int code){
 	for(;;){}
 }
 
-//LFramework::Thread ledThread;
+LFramework::Thread ledThread;
 
 void ledTask(){
 	__HAL_RCC_GPIOE_CLK_ENABLE();
@@ -82,7 +83,29 @@ struct Color24{
 
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
-UsbDevice usb(&hpcd_USB_OTG_FS);
+UsbInterface<int, float> itf0;
+
+
+template<typename ... Interfaces>
+struct UsbFunction{
+
+};
+
+typedef UsbInterface<int, float> CdcControlInterface;
+typedef UsbInterface<int, float> CdcDataInterface;
+
+struct UsbCdcFunction : public UsbFunction<CdcControlInterface, CdcDataInterface>{
+
+};
+
+typedef UsbConfiguration<UsbCdcFunction> C0;
+typedef UsbConfiguration<UsbCdcFunction> C1;
+
+
+C0 config0;
+C1 config1;
+
+UsbDevice<Color24, C0, C1> usb(&hpcd_USB_OTG_FS, &config0, &config1);
 
 
 TEST_IMPORT(BitField_8bit)
@@ -99,9 +122,9 @@ void mainThread(){
 	float3 right = f3.unitX();
 	float4 f4;
 
-	//LFramework::ThisThread::setPriority(LFramework::ThreadPriority::Normal);
+	LFramework::ThisThread::setPriority(LFramework::ThreadPriority::Normal);
 
-//	ledThread = std::move(LFramework::Thread("led", 256, LFramework::ThreadPriority::Normal, ledTask));
+	ledThread = std::move(LFramework::Thread("led", 256, LFramework::ThreadPriority::Normal, ledTask));
 
 	DisplayFsmcInterface::init();
 
@@ -114,6 +137,15 @@ void mainThread(){
 
 	Lcd::clear(color.toColor16());
 
+	usb.descriptor.classDescription = UsbClass::Device::VendorSpecific(0,0);
+	usb.descriptor.bMaxPacketSize0 = 64;
+	usb.descriptor.bNumConfigurations = 1;
+	usb.descriptor.bcdDevice = UsbVersion(1);
+	usb.descriptor.bcdUSB = UsbVersion(2);
+	usb.descriptor.idProduct = 0;
+	usb.descriptor.idVendor = 0x0302;
+	//usb.descriptor.
+
 	usb.initPHY();
 	usb.start();
 
@@ -121,6 +153,44 @@ void mainThread(){
 		vTaskDelay(1000);
 	}
 }
+
+extern "C" {
+	void HAL_PCD_DataOutStageCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum){
+		usb.onDataOut(epnum);
+	}
+	void HAL_PCD_DataInStageCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum){
+		usb.onDataIn(epnum);
+	}
+	void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef *hpcd){
+		usb.onSetup();
+	}
+	void HAL_PCD_SOFCallback(PCD_HandleTypeDef *hpcd){
+		usb.onSOF();
+	}
+	void HAL_PCD_ResetCallback(PCD_HandleTypeDef *hpcd){
+		usb.onReset();
+	}
+	void HAL_PCD_SuspendCallback(PCD_HandleTypeDef *hpcd){
+		usb.onSuspend();
+	}
+	void HAL_PCD_ResumeCallback(PCD_HandleTypeDef *hpcd){
+		usb.onResume();
+	}
+	void HAL_PCD_ISOOUTIncompleteCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum){
+		usb.onIsochronousOutIncomplete(epnum);
+	}
+	void HAL_PCD_ISOINIncompleteCallback(PCD_HandleTypeDef *hpcd, uint8_t epnum){
+		usb.onIsochronousInIncomplete(epnum);
+	}
+	void HAL_PCD_ConnectCallback(PCD_HandleTypeDef *hpcd){
+		usb.onConnect();
+	}
+	void HAL_PCD_DisconnectCallback(PCD_HandleTypeDef *hpcd){
+		usb.onDisconnect();
+	}
+}
+
+
 
 extern "C" void __libc_init_array (void);
 extern "C" void mainThreadLauncher(){
